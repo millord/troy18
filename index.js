77819162b9ffea2df940ada3afc34c9e7df5aa28
jmillod <juancarlosmillord@gmail.com>
2020-12-20 06:41:35 -0400

    first version

---
+++ index.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+console.log(`Juan Millord
+
+Voracious learning and student. In love with open source and JavaScript in
+general.
+
+Github: https://github.com/millord
+
+`);
