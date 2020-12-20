2607555b5b4f055405f723937226a4992142ba86
jmillod <juancarlosmillord@gmail.com>
2020-12-20 17:30:55 -0400

    changed the version and added clear console

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
+const clearConsole = require("clear-any-console");
 
+// clear console
+clearConsole();
 console.log(`Juan Millord
 
 Voracious learning and student. In love with open source and JavaScript in
